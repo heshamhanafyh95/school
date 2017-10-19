f1556df3ee28d86453c0c96dce0d2a9673ad1d03
AbdAllahEl-Gamal <abdualla.algamal.student@pua.edu.eg>
2017-10-20 00:28:19 +0200

    Edit

--- js/js.js
+++ js/js.js
@@ -9,166 +9,123 @@ var config = {
 firebase.initializeApp(config);
 
 var employeeapp = angular.module("employeeApp", ["firebase"]);
-
-employeeapp.factory("EmployeeSignUpForm", ["$firebaseArray",
-  function($firebaseArray) {
-
-    var ref = firebase.database().ref().child("employees");
-
-    return $firebaseArray(ref);
-  }
-]);
-
 var parentapp = angular.module("parentApp", ["firebase"]);
-
-parentapp.factory("ParentSignUpForm", ["$firebaseArray",
-  function($firebaseArray) {
-
-    var ref2 = firebase.database().ref().child("parents");
-
-    return $firebaseArray(ref2);
-  }
-]);
-
+var studentapp = angular.module("studentApp", ["firebase"]);
 var attendanceapp = angular.module("attendanceApp", ["firebase"]);
-
-attendanceapp.factory("AttendanceSignUpForm", ["$firebaseArray",
-  function($firebaseArray) {
-
-    var ref3 = firebase.database().ref().child("student/2/attendance");
-
-    return $firebaseArray(ref3);
-  }
-]);
-
 var gradesapp = angular.module("gradesApp", ["firebase"]);
 
-gradesapp.factory("GradesSignUpForm", ["$firebaseArray",
-  function($firebaseArray) {
+employeeapp.controller("EmployeeCtrl", ["$scope",
 
-	var ref4 = firebase.database().ref().child("student/2/grades");
-
-    return $firebaseArray(ref4);
-  }
+  function($scope) 
+  {
+	  var ref = firebase.database().ref();
+      $scope.employeeInformation = function() 
+	  {
+		  var usersRef = ref.child("employees/"+$scope.phonenumber);		
+          usersRef.set({
+			  firstname: $scope.firstname,
+              lastname: $scope.lastname,
+		      position: $scope.position,
+		      phonenumber: $scope.phonenumber,
+		      email: $scope.email,
+		      password: $scope.password
+			  });
+			  $scope.firstname = "";
+	          $scope.lastname = "";
+	          $scope.position = "";
+	          $scope.phonenumber = "";
+	          $scope.email = "";
+	          $scope.password = "";
+	  };
+    }
 ]);
 
+parentapp.controller("ParentCtrl", ["$scope",
 
-
-employeeapp.controller("EmployeeCtrl", ["$scope", "EmployeeSignUpForm",
-
-  function($scope, EmployeeSignUpForm) {
-
-    $scope.E = EmployeeSignUpForm;
-    
-    $scope.employeeInformation = function() {
-		
-      $scope.E.$add({
-		firstname: $scope.firstname,
-        lastname: $scope.lastname,
-		position: $scope.position,
-		phonenumber: $scope.phonenumber,
-		email: $scope.email,
-		password: $scope.password
-      });
-	  
-      $scope.firstname = "";
-	  $scope.lastname = "";
-	  $scope.position = "";
-	  $scope.phonenumber = "";
-	  $scope.email = "";
-	  $scope.password = "";
-    };
-
-  }
+  function($scope) 
+  {
+	  var ref = firebase.database().ref();
+      $scope.parentInformation = function() 
+	  {
+		  var usersRef = ref.child("parents/"+$scope.phonenumber);		
+          usersRef.set({
+			  firstname: $scope.firstname,
+              lastname: $scope.lastname,
+		      phonenumber: $scope.phonenumber,
+		      email: $scope.email,
+		      password: $scope.password
+			  });
+			  $scope.firstname = "";
+	          $scope.lastname = "";
+	          $scope.phonenumber = "";
+	          $scope.email = "";
+	          $scope.password = "";
+	  };
+    }
 ]);
 
-parentapp.controller("ParentCtrl", ["$scope", "ParentSignUpForm",
+studentapp.controller("StudentCtrl", ["$scope",
 
-  function($scope, ParentSignUpForm) {
-
-    $scope.P = ParentSignUpForm;
-
-    $scope.parentInformation = function() {
-
-      $scope.P.$add({
-        firstname: $scope.firstname,
-        lastname: $scope.lastname,
-		phonenumber: $scope.phonenumber,
-		email: $scope.email,
-		password: $scope.password
-      });
-	  
-      $scope.firstname = "";
-	  $scope.lastname = "";
-	  $scope.phonenumber = "";
-	  $scope.email = "";
-	  $scope.password = "";
-    };
-
-  }
+  function($scope) 
+  {
+	  var ref = firebase.database().ref();
+      $scope.studentInformation = function() 
+	  {
+		  var usersRef = ref.child("students/"+$scope.phonenumber);		
+          usersRef.set({
+			  firstname: $scope.firstname,
+              lastname: $scope.lastname,
+			  level: $scope.level,
+			  classroom: $scope.classroom,
+		      phonenumber: $scope.phonenumber,
+		      email: $scope.email,
+		      password: $scope.password
+			  });
+			  $scope.firstname = "";
+	          $scope.lastname = "";
+			  $scope.level = "";
+			  $scope.classroom = "";
+	          $scope.phonenumber = "";
+	          $scope.email = "";
+	          $scope.password = "";
+	  };
+    }
 ]);
 
-attendanceapp.controller("AttendanceCtrl", ["$scope", "AttendanceSignUpForm",
+attendanceapp.controller("AttendanceCtrl", ["$scope",
 
-  function($scope, AttendanceSignUpForm) {
-
-    $scope.A = AttendanceSignUpForm;
-
-    $scope.attendance = function() {
-
-      $scope.A.$add({
-        level: $scope.level,
-        classroom: $scope.classroom
-      });
-	  
-      $scope.level = "";
-	  $scope.classroom = "";
-    };
-
-  }
+  function($scope) 
+  {
+	  var ref = firebase.database().ref();
+      $scope.attendance = function() 
+	  {
+		  var usersRef = ref.child("attendance/"+$scope.level);		
+          usersRef.set({
+			  level: $scope.level,
+              classroom: $scope.classroom
+			  });
+			  $scope.level = "";
+	          $scope.classroom = "";
+	  };
+    }
 ]);
 
-gradesapp.controller("GradesCtrl", ["$scope", "GradesSignUpForm",
+gradesapp.controller("GradesCtrl", ["$scope",
 
-  function($scope, GradesSignUpForm) {
-
-    $scope.G = GradesSignUpForm;
-
-    $scope.grades = function() {
-
-      $scope.G.$add({
-        level: $scope.level,
-        classroom: $scope.classroom,
-		grade: $scope.grade
-      });
-	  
-      $scope.level = "";
-	  $scope.classroom = "";
-	  $scope.grade = "";
-    };
-
-  }
+  function($scope) 
+  {
+	  var ref = firebase.database().ref();
+      $scope.grades = function() 
+	  {
+		  var usersRef = ref.child("grades/"+$scope.level);		
+          usersRef.set({
+			  level: $scope.level,
+              classroom: $scope.classroom,
+		      grade: $scope.grade
+			  });
+			  $scope.level = "";
+	          $scope.classroom = "";
+	          $scope.grade = "";
+	  };
+    }
 ]);
-
-
-
-
-//firebase.auth().signInWithEmailAndPassword("schoolmobpua@gmail.com","pua123456").catch(function(error) {
-		  // Handle Errors here.
-		  //var errorCode = error.code;
-		  //var errorMessage = error.message;
-		  // ...
-		//});
-//function login(){
-	//var app = angular.module('db', ["firebase"]);
-	//app.controller('database', function($firebaseObject) {
-		
-		//var ref= firebase.database().ref.child("users/"+"3");
-		//ref.set({
-			//asdf{
-				//username:"hesham",
-				//phone:"015"
-			//}
-		//});
-	//});
-//}
